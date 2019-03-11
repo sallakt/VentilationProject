87d33f30e26da41d7e722795965443b86dfebd7e
Vladimir Moroz <vovanmoroz1509@gmail.com>
2019-03-11 18:59:31 +0200

    Added setFrequency as friend function to AutoCon class for sending data to modbus

--- src/AutoCon.cpp
+++ src/AutoCon.cpp
@@ -6,18 +6,59 @@
  */
 
 #include "AutonCon.h"
+#include "ITM_conv.h"
 
-AutoCon::AutoCon(I2CMaster* i2c)
+AutoCon::AutoCon(I2CMaster* i2c)//UNFINISHED
 {
 	uint8_t received[3];
 	i2c->ReadValueI2CM(received, 3);
 	baseVal = received[1];
 }
 
-AutoCon::adjust(I2CMaster* I2Cread)
+void AutoCon::adjust(I2CMaster* I2Cread, ModbusMaster* mbWrite)//UNFINISHED
 {
 	uint8_t received[3];
 	I2Cread->ReadValueI2CM(received, 3);
+	//READING VALUES FOR TESTING
+	p.print("\n Values: ");
+	p.print(received[0]);
+
+	p.print(" - ");
+	p.print(baseVal);
 }
 
 
+
+
+
+bool setFrequency(ModbusMaster& node, uint16_t freq, void (*Sleep)(int) )
+{
+	int result;
+	int ctr;
+	bool atSetpoint;
+	const int delay = 500;
+
+	ModbusRegister Frequency(&node, 1); // reference 1
+	ModbusRegister StatusWord(&node, 3);
+
+	Frequency = freq; // set motor frequency
+
+	//p.print("\nSet freq = %d"); // for debugging
+	//p.print((int) ctr * delay);
+
+
+	// wait until we reach set point or timeout occurs
+	ctr = 0;
+	atSetpoint = false;
+	do {
+		Sleep(delay);
+		// read status word
+		result = StatusWord;
+		// check if we are at setpoint
+		if (result >= 0 && (result & 0x0100)) atSetpoint = true;
+		ctr++;
+	} while(ctr < 20 && !atSetpoint);
+	//p.print("\n El:"); // for debugging
+	//p.print((int) ctr * delay);
+	return atSetpoint;
+}
--- src/AutonCon.h
+++ src/AutonCon.h
@@ -9,19 +9,26 @@
 #define AUTONCON_H_
 #include "I2C.h"
 #include "I2CMaster.h"
+#include "ModbusMaster.h"
+#include "ModbusRegister.h"
+#include "ITM_conv.h"
+#include "ITM_write.h"
 
 
-
+//UNFINISHED
 class AutoCon
 {
+	friend bool setFrequency(ModbusMaster& node, uint16_t freq, void (*Sleep)(int));
 public:
-	Autcon::AutoCon(I2CMaster* i2c);
+	AutoCon(I2CMaster* i2c);
 	virtual ~AutoCon(){};
-	void AutoCon::adjust(I2CMaster* I2Cread);
+	void adjust(I2CMaster* I2Cread, ModbusMaster* mbWrite);
+
 private:
 	uint8_t baseVal;
+	ITM_conv p; //FOR TESTING
 
 
-}
+};
 
 #endif /* AUTONCON_H_ */
